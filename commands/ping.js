c5bc64035540a0e7fa509d25262678ab4ad99ef7
RoCoderXD <exlgamingyt@gmail.com>
2023-01-29 17:08:10 +0000

    change ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('ping')
+    .setName('pinger')
     .setDescription('Gives you a good ol\' pong\'in'),
     async execute(interaction) {
         await interaction.reply('Pong!');
